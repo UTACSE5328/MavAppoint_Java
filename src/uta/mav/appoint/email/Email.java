bc21266f5072c36d9c093da947b67c10bb7d4b11
linc0814 <yzlinc0814@gmail.com>
2017-02-16 23:39:07 -0600

    first commit

---
+++ src/uta/mav/appoint/email/Email.java
@@ -0,0 +1,55 @@
+package uta.mav.appoint.email;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.*;
+
+public class Email {
+	private String subject;
+	private String text;
+	private String toEmail;
+	
+	public Email(String sub, String txt, String destEmail)
+	{
+		subject = sub;
+		text = txt;
+		toEmail = destEmail;
+	}
+	public void sendMail()
+	{
+		final String user = "cse5328sp2016@gmail.com";
+        final String pass = "seproject";
+ 
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+ 
+        Session session = Session.getInstance(props,
+          new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, pass);
+            }
+          });
+ 
+        try {
+ 
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("cse5328sp2016@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                InternetAddress.parse(toEmail));
+            message.setSubject(subject);
+            //message.setText(text);
+            message.setContent(text, "text/html");
+ 
+            Transport.send(message);
+ 
+            System.out.println("1. Email Sent");
+ 
+        } 
+		catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+	}
+}
